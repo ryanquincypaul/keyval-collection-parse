381a8fb634b9f3267bdc7f374c2ac3ea9cfe966d
ryanquincypaul <littlefoot.paul@gmail.com>
2016-09-14 17:33:33 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject keyval-collection-parse "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Clojure library for parsing a list of an attribute vector and value vectors into a vector of maps."
+  :url "https://github.com/ryanquincypaul/keyval-collection-parse"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
